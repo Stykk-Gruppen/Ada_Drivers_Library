f827919b5858ac28602d8744c9e926eae22c92a4
Kent Odde <kentodde89@gmail.com>
2020-09-18 10:19:40 +0200

    testCommit

--- boards/Arduino_Nano_33_BLE_Sense/src/nrf52_dk-ios.ads
+++ boards/Arduino_Nano_33_BLE_Sense/src/nrf52_dk-ios.ads
@@ -28,7 +28,7 @@
 --   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   --
 --                                                                          --
 ------------------------------------------------------------------------------
-
+j
 with nRF.Device; use nRF.Device;
 with nRF.GPIO; use nRF.GPIO;
 
